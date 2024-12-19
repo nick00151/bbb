a09aa7327820a14e6a2080badadae3d212818ebe
nick00151 <nick00151@gmail.com>
2024-12-19 13:31:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-return render_template('index.html')
-@app.route('/hello'
-, methods=['POST'])
+    return render_template('index.html')  # 需要正確縮排
+
+@app.route('/hello', methods=['POST'])
 def hello():
-return jsonify(message =
-'Hello World !')
-if __name__ ==
-'__main__':
-app.run(debug=True)
+    return jsonify(message='Hello World!')  # 需要正確縮排
+
+if __name__ == '__main__':
+    app.run(debug=True)  # 需要正確縮排
+
