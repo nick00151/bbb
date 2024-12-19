0e03547f7e34e918fcb704521192a6993565f2a0
nick00151 <nick00151@gmail.com>
2024-12-19 15:45:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 from openc import OpenCC
 import openai
+from langchain_community.vectorstores import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
